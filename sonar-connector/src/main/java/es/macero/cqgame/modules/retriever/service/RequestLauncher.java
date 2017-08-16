ea0968eae65ea8abb8b6027cc258771007288a80
Michael Ecker <mecker@next-level-integration.com>
2017-08-16 18:33:05 +0200

    refactoring for re-usage with other http-requests

---
+++ sonar-connector/src/main/java/es/macero/cqgame/modules/retriever/service/RequestLauncher.java
@@ -0,0 +1,72 @@
+package es.macero.cqgame.modules.retriever.service;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import es.macero.cqgame.modules.sonarapi.resultbeans.Issue;
+import es.macero.cqgame.modules.sonarapi.resultbeans.Issues;
+import es.macero.cqgame.modules.sonarapi.resultbeans.Paging;
+import es.macero.cqgame.modules.stats.service.SonarStatsService;
+import es.macero.cqgame.util.ApiHttpUtils;
+
+abstract class RequestLauncher implements Consumer<String> {
+
+	private static final Log log = LogFactory.getLog(RequestLauncher.class);
+	private SonarStatsService statsService;
+	private String sonarUser;
+	private String sonarPassword;
+
+	RequestLauncher(final SonarStatsService statsService, final String sonarUser, final String sonarPassword) {
+		this.statsService = statsService;
+		this.sonarUser = sonarUser;
+		this.sonarPassword = sonarPassword;
+	}
+
+	@Override
+	public void accept(final String id) {
+		try {
+			int pageIndex = 1;
+			Integer totalPages = 1;
+			List<Issue> issues = new ArrayList<>();
+			while (pageIndex <= totalPages) {
+				RestTemplate restTemplate = new RestTemplate();
+				HttpEntity<String> request = new HttpEntity<>(getHeaders());
+				URI uri = getUrl(id, pageIndex);
+				ResponseEntity<Issues> response = restTemplate.exchange(uri, HttpMethod.GET, request, Issues.class);
+				if (pageIndex == 1) {
+					Paging paging = response.getBody().getPaging();
+					totalPages = (Integer) paging.getAdditionalProperties().get("pages");
+				}
+				issues.addAll(response.getBody().getIssues());
+				pageIndex++;
+			}
+			process(id, issues);
+		} catch (final HttpServerErrorException serverException) {
+			log.error(serverException);
+			throw serverException;
+		}
+	}
+
+	private HttpHeaders getHeaders() {
+		if (StringUtils.isNotBlank(sonarUser)) {
+			return ApiHttpUtils.getHeaders(sonarUser, sonarPassword);
+		}
+		return new HttpHeaders();
+	}
+
+	abstract URI getUrl(final String assignee, final int pageIndex);
+
+	abstract void process(final String id, List<Issue> issues);
+}
--- sonar-connector/src/main/java/es/macero/cqgame/modules/retriever/service/SonarDataRetriever.java
+++ sonar-connector/src/main/java/es/macero/cqgame/modules/retriever/service/SonarDataRetriever.java
@@ -1,33 +1,21 @@
 package es.macero.cqgame.modules.retriever.service;
 
-import es.macero.cqgame.modules.configuration.service.SonarServerConfigurationService;
-import es.macero.cqgame.modules.sonarapi.resultbeans.Issue;
-import es.macero.cqgame.modules.sonarapi.resultbeans.Issues;
-import es.macero.cqgame.modules.sonarapi.resultbeans.Paging;
-import es.macero.cqgame.modules.stats.service.SonarStatsService;
-import es.macero.cqgame.util.ApiHttpUtils;
+import java.net.URI;
+import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
+import es.macero.cqgame.modules.configuration.domain.SonarServerConfiguration;
+import es.macero.cqgame.modules.configuration.service.SonarServerConfigurationService;
+import es.macero.cqgame.modules.sonarapi.resultbeans.Issue;
+import es.macero.cqgame.modules.stats.service.SonarStatsService;
 
 @Service
 @EnableAsync
@@ -38,78 +26,38 @@ final class SonarDataRetriever {
 	private static final String GET_ISSUES_COMMAND = "/api/issues/search?assignees={assignees}&resolutions={resolutions}&p={page}&ps={pageSize}";
 
 	private final SonarStatsService statsService;
-	private final SonarServerConfigurationService configurationService;
+	private final SonarServerConfiguration configuration;
 
 	@Autowired
-	public SonarDataRetriever(final SonarStatsService statsService, final SonarServerConfigurationService configurationService) {
+	public SonarDataRetriever(final SonarStatsService statsService,
+			final SonarServerConfigurationService configurationService) {
 		this.statsService = statsService;
-		this.configurationService = configurationService;
+		this.configuration = configurationService.getConfiguration();
 	}
 
 	@Scheduled(fixedRate = 10 * 60000)
 	public void retrieveData() {
-		// It seems that sonar doesn't allow parallel queries with same user since it creates a register for internal
-		// stats and that causes an error when inserting into the database.
-		statsService.getIds().forEach(
-				new RequestLauncher(statsService, configurationService.getConfiguration().getUrl(),
-						configurationService.getConfiguration().getUser(),
-						configurationService.getConfiguration().getPassword()));
-	}
+		String sonarUrl = configuration.getUrl();
+		RequestLauncher requestLauncher = new RequestLauncher(statsService, configuration.getUser(),
+				configuration.getPassword()) {
+			@Override
+			URI getUrl(final String assignee, final int pageIndex) {
+				URI uri = UriComponentsBuilder.fromHttpUrl(sonarUrl + GET_ISSUES_COMMAND)
+						.buildAndExpand(assignee.toLowerCase() + "," + assignee.toUpperCase(), "FIXED", pageIndex, 500)
+						.toUri();
+				log.debug("generated URI " + uri);
+				return uri;
+			}
 
-	private static final class RequestLauncher implements Consumer<String> {
-
-		private SonarStatsService statsService;
-		private String sonarUrl;
-		private String sonarUser;
-		private String sonarPassword;
-
-		RequestLauncher(final SonarStatsService statsService, final String sonarUrl, final String sonarUser, final String sonarPassword) {
-			this.statsService = statsService;
-			this.sonarUrl = sonarUrl;
-			this.sonarUser = sonarUser;
-			this.sonarPassword = sonarPassword;
-		}
-
-		@Override
-		public void accept(final String id) {
-			try {
-				int pageIndex = 1;
-				Integer totalPages = 1;
-				List<Issue> issues = new ArrayList<>();
-				while (pageIndex <= totalPages) {
-					RestTemplate restTemplate = new RestTemplate();
-					HttpEntity<String> request = new HttpEntity<>(getHeaders());
-					URI uri = getResolvedIssuesForAssignee(id, pageIndex);
-					ResponseEntity<Issues> response = restTemplate.exchange(uri, HttpMethod.GET, request, Issues.class);
-					if (pageIndex == 1) {
-						Paging paging = response.getBody().getPaging();
-						totalPages = (Integer) paging.getAdditionalProperties().get("pages");
-					}
-					issues.addAll(response.getBody().getIssues());
-					pageIndex++;
-				}
+			@Override
+			void process(final String id, List<Issue> issues) {
 				statsService.updateStats(id, issues);
-			} catch (final HttpServerErrorException serverException) {
-				log.error(serverException);
-				throw serverException;
 			}
-		}
-
-		HttpHeaders getHeaders() {
-			if (StringUtils.isNotBlank(sonarUser)) {
-				return ApiHttpUtils.getHeaders(sonarUser, sonarPassword);
-			} else {
-				return new HttpHeaders();
-			}
-		}
-
-		URI getResolvedIssuesForAssignee(final String assignee, final int pageIndex) {
-			URI uri = UriComponentsBuilder.fromHttpUrl(sonarUrl + GET_ISSUES_COMMAND)
-				.buildAndExpand(assignee.toLowerCase() + "," + assignee.toUpperCase(), "FIXED", pageIndex, 500)
-				.toUri();
-			log.debug("generated URI " + uri);
-			return uri;
-		}
+		};
+		// It seems that sonar doesn't allow parallel queries with same user since it
+		// creates a register for internal
+		// stats and that causes an error when inserting into the database.
+		statsService.getIds().forEach(requestLauncher);
 	}
 
 }
