abbce461a30ec9cf247a08705e730e09cb425008
Michael Ecker <mecker@next-level-integration.com>
2017-08-20 22:14:56 +0200

    improved second test

---
+++ sonar-connector/src/main/java/es/macero/cqgame/modules/retriever/service/SonarIssueAssignerImpl.java
@@ -0,0 +1,123 @@
+package es.macero.cqgame.modules.retriever.service;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import es.macero.cqgame.modules.configuration.domain.SonarServerConfiguration;
+import es.macero.cqgame.modules.configuration.service.SonarServerConfigurationService;
+import es.macero.cqgame.modules.sonarapi.resultbeans.Issue;
+
+@Service
+@Component
+public class SonarIssueAssignerImpl implements SonarIssueAssigner {
+
+	private static final String GET_ISSUES_FROM_CLASS = "/api/issues/search?componentKeys={projectId}:{moduleId}:{classPath}&statuses={status}&assigned={assigned}";
+	private static final String POST_ISSUES_ASSIGN = "/api/issues/bulk_change?actions={actions}&assign.assignee={assignee}&issues={issueKey}";
+	private static Map<String, String> mapping;
+	private SonarServerConfiguration configuration;
+	private RestTemplate restTemplate;
+	private List<String> issueKeys = new ArrayList<>();
+
+	static {
+		Map<String, String> map = new HashMap<>();
+		map.put("edi.validation", "com.nextlevel.editools");
+		map.put("edi.resources", "com.nextlevel.editools");
+		map.put("edi.generator", "com.nextlevel.editools");
+		map.put("analyzerFunctionalView", "com.nextlevel.editools");
+		mapping = Collections.unmodifiableMap(map);
+	}
+
+	@Autowired
+	public SonarIssueAssignerImpl(final SonarServerConfigurationService configurationService, final RestTemplate restTemplate) {
+		this.configuration = configurationService.getConfiguration();
+		this.restTemplate = restTemplate;
+	}
+
+	/* (non-Javadoc)
+	 * @see es.macero.cqgame.modules.retriever.service.SonarIssueAssigner#assignIssues(java.lang.String)
+	 */
+	@Override
+	public void assignIssues(String classname) {
+		processSingleClass(classname);
+	}
+
+	private void processSingleClass(String classname) {
+		for (Entry<String, String> mapElement : mapping.entrySet()) {
+			if (StringUtils.contains(classname, mapElement.getKey())) {
+				processProject(mapElement.getValue(), mapElement.getKey(), classname);
+			}
+		}
+	}
+
+	private void processProject(String projectId, String moduleId, String classname) {
+		String sonarUrl = "http://ci.next-level-integration.com/sonar/";
+		String user = configuration.getUser();
+		String passwd = configuration.getPassword();
+		RequestLauncher launcher = new RequestLauncher(user, passwd, restTemplate) {
+
+			@Override
+			public void process(String id, List<Issue> issues) {
+				issueKeys = issues.stream().map(Issue::getKey).collect(Collectors.toList());
+				System.err.println(issueKeys);
+				// for (Issue issue : issues) {
+				// System.err.println(issue);
+				// issueKeys.add(issue.getKey());
+				// }
+			}
+
+			@Override
+			public URI getUrl(String assignee, int pageIndex) {
+				System.err.println(assignee);
+				System.err.println(pageIndex);
+				String issueStatus = "OPEN";
+				String isAssigned = "false";
+				URI uri = UriComponentsBuilder.fromHttpUrl(sonarUrl + GET_ISSUES_FROM_CLASS)
+						.buildAndExpand("assign", moduleId,
+								"src/main/java/com/nextlevel/editools/edi/validation/ClonerFactory.java", issueStatus,
+								isAssigned)
+						.toUri();
+				System.err.println(uri);
+				return uri;
+			}
+		};
+		launcher.accept("mecker");
+		System.err.println(issueKeys);
+		RequestLauncher launcher2 = new RequestLauncher(user, passwd, restTemplate) {
+
+			@Override
+			public void process(String id, List<Issue> issues) {
+				// TODO Auto-generated method stub
+				System.err.println(id);
+				System.err.println(issues);
+			}
+
+			@Override
+			public URI getUrl(String assignee, int pageIndex) {
+				URI uri = UriComponentsBuilder.fromHttpUrl(sonarUrl + POST_ISSUES_ASSIGN)
+						.buildAndExpand("assign", "mecker", "38c32639-f2c9-45de-8d30-0ee73582f73a").toUri();
+				System.err.println(uri);
+				return uri;
+			}
+		};
+		launcher2.setMethod(HttpMethod.POST);
+		launcher2.accept("mecker");
+	}
+}
---
+++ sonar-connector/src/test/java/es/macero/cqgame/modules/retriever/service/SonarIssueAssignerTest.java
@@ -0,0 +1,52 @@
+package es.macero.cqgame.modules.retriever.service;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import es.macero.cqgame.modules.configuration.domain.SonarServerConfiguration;
+import es.macero.cqgame.modules.configuration.service.SonarServerConfigurationService;
+import es.macero.cqgame.modules.sonarapi.resultbeans.Issues;
+
+public class SonarIssueAssignerTest {
+
+	@Mock
+	SonarServerConfigurationService configurationService;
+
+	@Mock
+	RestTemplate restTemplate;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void test() {
+		when(configurationService.getConfiguration()).thenReturn(new SonarServerConfiguration(null, null, null));
+		Issues body = new Issues();
+		ResponseEntity<Issues> response = new ResponseEntity<>(body, HttpStatus.OK);
+		when(restTemplate.exchange(any(URI.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(Issues.class)))
+				.thenReturn(response);
+		SonarIssueAssigner issueAssigner = new SonarIssueAssignerImpl(configurationService, restTemplate);
+		issueAssigner.assignIssues("analyzerFunctionalView-myClass");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
